69903d42bd355f22736bf58d86bff51e32fb0d16
JakubKub13 <jakubkubala3@gmail.com>
2023-01-01 00:48:43 +0100

    Finishing python file

--- Text_generation.py
+++ Text_generation.py
@@ -27,3 +27,14 @@ pprint(model(lines[0], num_return_sequences=3, max_length=20))
 def wrap(x):
   return textwrap.fill(x, replace_whitespace=False, fix_sentence_endings=True)
 
+out = model(lines[0], max_length=30)
+print(wrap(out[0]['generated_text']))
+
+previous = "Two roads diverged in a yellow wood, including one that blocked the road leading to another intersection in the middle of the city"
+out = model(previous + '\n' + lines[2], max_length=60)
+print(wrap(out[0]['generated_text']))
+
+# Exercise: think of ways you would apply GPT-2 and text generation in the real world and as a businnes / startup
+prompt = "Neural networks with attention have been used with great success in natural language processing."
+out = model(prompt, max_length=300)
+print(wrap(out[0]['generated_text']))
